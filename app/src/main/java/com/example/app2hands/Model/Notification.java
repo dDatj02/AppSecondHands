d4421853f25b922a3642937d8f881c744201e484
Lam Dat <lamgiadat2231@gmail.com>
2022-12-16 23:41:53 +0700

    Update Notification

--- app/src/main/java/com/example/app2hands/Model/Notification.java
+++ app/src/main/java/com/example/app2hands/Model/Notification.java
@@ -1,6 +1,9 @@
 package com.example.app2hands.Model;
 
-public class Notification {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Notification implements Parcelable {
     String title, description;
 
     public Notification() {
@@ -10,6 +13,23 @@ public class Notification {
         this.title = title;
         this.description = description;
     }
+
+    protected Notification(Parcel in) {
+        title = in.readString();
+        description = in.readString();
+    }
+
+    public static final Creator<Notification> CREATOR = new Creator<Notification>() {
+        @Override
+        public Notification createFromParcel(Parcel in) {
+            return new Notification(in);
+        }
+
+        @Override
+        public Notification[] newArray(int size) {
+            return new Notification[size];
+        }
+    };
 
     public String getTitle() {
         return title;
@@ -25,5 +45,16 @@ public class Notification {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(description);
     }
 }
